15745d973dbea7d68264b24110f0df6dcfc63f1f
Plasnid <stuart@sackler.ca>
2020-09-14 16:10:54 -0400

    first commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+/**
+ * * To run this file, open your terminal and type:
+ * 
+ * ! npm run start_js_only
+ * 
+ * * npm will find the taskrunner called start_js_only and run it
+ * * The taskrunner will get node to run your javascript file.
+ * 
+ * * It only runs your javascript file once though...
+ * * How do we solve this?  
+ */
+
+console.log(`node can run javascript files!`);
